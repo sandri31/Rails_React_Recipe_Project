efbe83a0de17df2177abc7ae9640cb5a9c20746e
Florent S <sandri822@hotmail.fr>
2023-12-14 14:35:16 +0100

    🔧 Add API routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes/show'
+      delete '/destroy/:id', to: 'recipes#destroy'
+    end
+  end
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
