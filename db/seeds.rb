ab51ecbd5663c366a32151e0873d90e913d63c6a
Florent S <sandri822@hotmail.fr>
2023-12-15 09:28:21 +0100

    🔧 Update recipe names and print statement

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
 9.times do |i|
   Recipe.create(
-    name: "Recipe #{i + 1}",
+    name: "Recette #{i + 1}",
     ingredients: 'Pot de 227 g de crème caillée, 25 g de beurre, 1 cuillère à café de maïzena, 100 g de parmesan, muscade râpée, 250 g de fettuccine ou tagliatelles fraîches, ciboulette ciselée ou persil haché pour servir (facultatif)',
     instruction: 'Dans une casserole moyenne, mélanger la crème caillée, le beurre et la maïzena à feu doux et laisser mijoter. Éteignez le feu et gardez au chaud.'
   )
 end
-puts '9 recipes created'
+puts '9 recettes ont été créées'
